2d1724534ea28da185c0e0421542c8fa5ab56044
sofikulislam7279 <sofikulislam7279@gmail.com>
2026-08-24 15:25:20 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roundup",
+    version="0.0.0",
+    author="Sofikul Islam",
+    author_email="sofikulislam7279@gmail.com",
+    packages=find_packages()
+)
+
+
+
