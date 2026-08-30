1f82fcf03173ccbb74c290a6ea93794ad13580a4
sofikulislam7279 <sofikulislam7279@gmail.com>
2026-08-31 00:06:56 +0600

    exception added

--- roundup/exception/__init__.py
+++ roundup/exception/__init__.py
@@ -15,7 +15,7 @@ def error_message_detail(error, error_detail:sys):
 class RoundupException(Exception):
     def __init__(self, error_message, error_detail):
         """
-        :param error_message: error message in string format
+        :param error_message: error message in string format.
         """
 
         super().__init__(error_message)
