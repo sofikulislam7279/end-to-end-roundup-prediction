43faa06f27b7de4d1e38cc2d60077c2d3a18474f
sofikulislam7279 <sofikulislam7279@gmail.com>
2026-09-05 13:52:23 +0600

    Data Transformation done

--- roundup/pipeline/training_pipeline.py
+++ roundup/pipeline/training_pipeline.py
@@ -3,13 +3,16 @@ from roundup.exception import RoundupException
 from roundup.logger import logging
 from roundup.components.data_ingestion import DataIngestion
 from roundup.components.data_validation import DataValidation
-from roundup.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
-from roundup.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
+from roundup.components.data_transformation import DataTransformation
+
+from roundup.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
+from roundup.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact)
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -56,6 +59,25 @@ class TrainingPipeline:
             raise RoundupException(e, sys) from e
 
 
+
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+
+        """
+        This method of TrainingPipeline class is responsible for starting data transformation component
+        """
+
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        
+        except Exception as e:
+            raise RoundupException(e, sys)
+        
+
+
     def run_pipeline(self) -> None:
         """
         This method of TrainingPipeline class is responsible for running complete pipeline
@@ -64,6 +86,8 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
 
         except Exception as e:
             raise RoundupException(e, sys)
--- roundup/utils/main_utils.py
+++ roundup/utils/main_utils.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import dill
 import yaml
-from pandas  import DataFrame
+from pandas import DataFrame
 
 from roundup.exception import RoundupException
 from roundup.logger import logging
@@ -17,7 +17,6 @@ def read_yaml_file(file_path: str) -> dict:
 
     except Exception as e:
         raise RoundupException(e, sys) from e
-    
 
 
 def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
@@ -25,11 +24,9 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         if replace:
             if os.path.exists(file_path):
                 os.remove(file_path)
-
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w") as file:
             yaml.dump(content, file)
-
     except Exception as e:
         raise RoundupException(e, sys) from e
 
@@ -38,6 +35,7 @@ def load_object(file_path: str) -> object:
     logging.info("Entered the load_object method of utils")
 
     try:
+
         with open(file_path, "rb") as file_obj:
             obj = dill.load(file_obj)
 
@@ -48,20 +46,17 @@ def load_object(file_path: str) -> object:
     except Exception as e:
         raise RoundupException(e, sys) from e
 
-
 def save_numpy_array_data(file_path: str, array: np.array):
     """
-    Save numpy array to file
+    Save numpy array data to file
     file_path: str location of file to save
     array: np.array data to save
     """
-
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(file_path, exist_ok=True)
-        with open(file_path, "wb") as file_obj:
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
             np.save(file_obj, array)
-
     except Exception as e:
         raise RoundupException(e, sys) from e
 
@@ -72,13 +67,11 @@ def load_numpy_array_data(file_path: str) -> np.array:
     file_path: str location of file to load
     return: np.array data loaded
     """
-
     try:
-        with open(file_path, "rb") as file_obj:
+        with open(file_path, 'rb') as file_obj:
             return np.load(file_obj)
-
     except Exception as e:
-        raise RoundupException(e, sys) from e  
+        raise RoundupException(e, sys) from e
 
 
 def save_object(file_path: str, obj: object) -> None:
@@ -95,19 +88,20 @@ def save_object(file_path: str, obj: object) -> None:
         raise RoundupException(e, sys) from e
 
 
-def drop_columns(df: DataFrame, cols: list) -> DataFrame:
+def drop_columns(df: DataFrame, cols: list)-> DataFrame:
+
     """
     drop the columns form a pandas DataFrame
     df: pandas DataFrame
     cols: list of columns to be dropped
     """
-    logging.info("Entered drop_columns method of utils")
+    logging.info("Entered drop_columns methon of utils")
 
     try:
         df = df.drop(columns=cols, axis=1)
 
         logging.info("Exited the drop_columns method of utils")
+        
         return df
-
     except Exception as e:
         raise RoundupException(e, sys) from e
